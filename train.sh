f4b03205cd5937ad769e9f75cb2e85eb9606ea7f
Alexander Morosow <alex@refikanadolstudio.com>
2025-01-13 11:12:03 -0800

    make train.sh a bit nicer

--- train.sh
+++ train.sh
@@ -1,9 +1,56 @@
-docker run --gpus all --shm-size=96g -it --rm \
-  -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch -v /media/Datauniverse:/datasets \
+#!/bin/bash
+
+# DATASET=/path/to/filelist.txt
+# RESUME=/path/to/resume.pkl
+
+docker run --gpus all --ipc host -it --rm \
+  -w /scratch \
+  -e HOME=/scratch \
+  -v $(pwd):/scratch \
+  -v /media/Datauniverse:/media/Datauniverse \
+  -v /raid/alex/data:/raid/alex/data \
+  -v /raid/cache/:/raid/cache \
+  --user $(id -u):$(id -g) \
   stylegan3 \
   python train.py \
-  --outdir=./results --cfg=stylegan3-r --data=/datasets/coral/data/filelists/coral_clean_rel.txt \
-  --gpus=4 --batch=32 --workers=16 --metrics=none --snap=10 --gamma=0 --nobench=True \
-  --mirror=1 --seed=0 --aug=noaug
-#   --mirror=1 --aug=ada --target=.6 --seed=0
-
+  --cfg=stylegan2 \
+  --metrics=none \
+  --nobench=False \
+  --outdir=./results \
+  --data=$DATASET \
+  --gpus=4 \
+  --snap=20 \
+  --gamma=10 \
+  --mirror=1 --seed=42 --aug=ada \
+  --mbstd-group=4 --batch=32 --batch-gpu=8 --workers=32 --prefetch=16 \
+  --resolution=1024 \
+  --kimg=40000
+#   --resume_kimg=0 \
+#   --resume=$RESUME \
+#   --glr .001 --dlr .001 \ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
